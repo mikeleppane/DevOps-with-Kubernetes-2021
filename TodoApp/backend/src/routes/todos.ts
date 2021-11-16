1624726b520429dc66a1d50f18db3d9baeae228e
Mikko Leppänen <mleppan23@gmail.com>
2021-11-16 13:17:59 +0200

    Exercise 2.10, Project v1.3: Changes: * Added character limit for the todo. * Enhanced logging for grafana monitoring.
    Signed-off-by: Mikko Leppänen <mleppan23@gmail.com>

--- TodoApp/backend/src/routes/todos.ts
+++ TodoApp/backend/src/routes/todos.ts
@@ -3,6 +3,10 @@ import { Todo } from "../database/models/todo";
 
 const todoRouter = express.Router();
 
+const isTodoTextValid = (text: string) => {
+  return text.length < 141;
+};
+
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 todoRouter.get("/", async (_req, res) => {
   const todos = await Todo.findAll();
@@ -14,6 +18,10 @@ todoRouter.get("/", async (_req, res) => {
 todoRouter.post("/", async (req, res) => {
   const body = req.body as { text: string; done: boolean };
   if (body.text) {
+    if (!isTodoTextValid(body.text)) {
+      console.log("Given todo text is too long. The limit is 140 characters.");
+      return res.status(400).json({ error: "Too many characters in todo" });
+    }
     const todo = { text: body.text, done: false };
     try {
       const createdTodo = await Todo.create(todo);
